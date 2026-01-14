06fb0efaf9785bff73f9dbc1402b4f0dad1ce5df
Christopher M. Judd <javajudd@gmail.com>
2026-01-14 07:28:00 -0500

    Added a PIR motion sensor example.

---
+++ code/PIR_Motion.ino
@@ -0,0 +1,25 @@
+#define PIR_PIN D5 // GPIO14 (D5)
+#define LED D2 // GPIO4 (D2)
+
+void setup() {
+  Serial.begin(115200);
+  delay(200);
+  pinMode(PIR_PIN, INPUT);
+  pinMode(LED, OUTPUT);
+  Serial.println("PIR Motion Sensor Ready");
+  Serial.println("Warming up sensor (30-60 seconds)...");
+}
+
+void loop() {
+  int motion = digitalRead(PIR_PIN);
+
+  if (motion == HIGH) {
+    digitalWrite(LED, HIGH);
+    Serial.println("Motion detected!");
+  } else {
+    digitalWrite(LED, LOW);
+    Serial.println("No motion");
+  }
+
+  delay(100);
+}
